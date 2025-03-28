e6b735ae001278f2003528164fe1fb4389991259
edrobi <ekdrobinski@gmail.com>
2025-03-27 19:13:12 -0700

    Initial commit of portfolio

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ATSOptimizedPortfolio from './components/ATSOptimizedPortfolio';
+
+function App() {
+  return <ATSOptimizedPortfolio />;
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import ATSOptimizedPortfolio from './components/ATSOptimizedPortfolio';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ATSOptimizedPortfolio />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
